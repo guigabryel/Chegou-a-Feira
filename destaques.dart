622b34771c2f8e35754110eb76ac0cdf216f10d3
guigabryel <152883843+guigabryel@users.noreply.github.com>
2024-06-05 16:59:08 -0300

    Update destaques.dart

--- destaques.dart
+++ destaques.dart
@@ -1,10 +1,202 @@
+import 'package:chegou_a_feira/barra.dart';
 import 'package:flutter/material.dart';
 
-class Destaques extends StatelessWidget {
+class Destaques extends StatefulWidget {
   const Destaques({super.key});
 
   @override
+  State<Destaques> createState() => _DestaquesState();
+}
+
+class _DestaquesState extends State<Destaques> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+            appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 176, 39, 69),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu, color: Colors.white,),
+        ),
+        actions: [
+          login(),
+          SizedBox(width: 18,),
+          cadastro(),
+          SizedBox(width: 10,),
+          
+        ],
+      ),
+            body: SingleChildScrollView(
+              child:  Column(
+              children: [
+                  Container(
+              width: MediaQuery.of(context).size.width,
+              height: 150,
+              color: Color.fromARGB(255, 81, 44, 99),
+              child: Row(
+                children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => Destaques()),
+                  );
+                        },
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: 130,
+                height: 130,
+              ),
+                      ),
+                  SizedBox(width: 80),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Meu carrinho',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    'R\$0,00',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+
+              
+              SizedBox(width: 100, child: SearchBar(),),
+                ],
+               ),      
+        ],
+       ),
+      ),
+      Container(
+         width: MediaQuery.of(context).size.width,
+         height: 70,
+         color: Color.fromARGB(255, 12, 12, 12),
+         child: 
+         Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('DESTAQUES',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+            ),
+           ),
+          ],
+         ),
+      ),
+      Row(
+        children: [
+          Container(
+            width: 200,
+            height: 350,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              border: Border.all(
+                color: Colors.blueGrey,
+                width: 1
+              )
+            ),
+            
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/pbsoft.png')
+              ],
+            ),
+          ),
+          Container(
+            width: 192.7,
+            height: 350,
+            color: Colors.blue,
+          ),
+        ],
+      ),
+     ],
+    ),
+   ),
+  );
+ }
+}
+
+
+
+class Barra extends StatelessWidget {
+  const Barra({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+        return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 176, 39, 69),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu, color: Colors.white,),
+        ),
+        actions: [
+          login(),
+          SizedBox(width: 18,),
+          cadastro(),
+          SizedBox(width: 10,),
+        ],
+      ),
+);
+  }
+}
+
+
+class SearchBar extends StatefulWidget {
+  @override
+  _SearchBarState createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _controller,
+      decoration: InputDecoration(
+        hintText: 'Digite o que está procurando',
+        hintStyle: TextStyle(color: Colors.grey), // Cor do texto do hint
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+          borderSide: BorderSide.none,
+        ),
+        filled: true,
+        fillColor: Color.fromARGB(255, 46, 45, 45), // Cor de fundo
+        contentPadding: EdgeInsets.symmetric(horizontal: 16.0), // Espaçamento interno
+        suffixIcon: Container(
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 110, 66, 146),
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(8.0),
+              bottomRight: Radius.circular(8.0),
+            ),
+          ),
+          child: Icon(Icons.search, color: Colors.white,), // Ícone de pesquisa
+        ),
+      ),
+      onChanged: (value) {
+        // Aqui você pode adicionar a lógica para pesquisar conforme o usuário digita
+        print('Pesquisando por: $value');
+      },
+      style: TextStyle(color: Colors.white), // Cor do texto
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
