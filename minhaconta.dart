40a3078add9c0c952f38723952392a7a657e0942
guigabryel <152883843+guigabryel@users.noreply.github.com>
2024-06-27 11:13:19 -0300

    Update minhaconta.dart

--- minhaconta.dart
+++ minhaconta.dart
@@ -1,10 +1,14 @@
+import 'package:chegou_a_feira/abas%20do%20perfil/alertas.dart';
+import 'package:chegou_a_feira/abas%20do%20perfil/favoritos.dart';
 import 'package:chegou_a_feira/components/barra.dart';
 import 'package:chegou_a_feira/components/abainicial.dart';
+import 'package:chegou_a_feira/components/barradoapp.dart';
 import 'package:chegou_a_feira/components/barralateral.dart';
 import 'package:chegou_a_feira/components/fim.dart';
 import 'package:chegou_a_feira/destaques.dart';
 import 'package:chegou_a_feira/login.dart';
 import 'package:chegou_a_feira/carrinho.dart';
+import 'package:chegou_a_feira/abas%20do%20perfil/prateleira.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -19,19 +23,20 @@ class _ContaState extends State<Conta> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       appBar: AppBar(
-              backgroundColor: Color.fromARGB(255, 176, 39, 69),
+      backgroundColor: Color.fromARGB(255, 176, 39, 69),
               iconTheme: IconThemeData(
                 color: Colors.white
               ),
-        
-        actions: [
+               actions: [
           login(),
           SizedBox(width: 18,),
           cadastro(),
           SizedBox(width: 10,),
-        ]
-      ),
+         ]
+        ),
+
       drawer: Barralateral(),      
 
             body: SingleChildScrollView(
@@ -72,53 +77,119 @@ class _ContaState extends State<Conta> {
                         
             Divider(),
 
+Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+              onTap: () {
+            Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => Prateleira()),
+                );
+              },
+                child: Container(
+                  width: 60,
+                  child: Image.asset('assets/images/estrela.png'),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+              onTap: () {
+            Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => Favoritos()),
+                );
+              },
+                child: Container(
+                  width: 60,
+                  child: Image.asset('assets/images/coracao.png'),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+              onTap: () {
+            Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => Alertas()),
+                );
+              },
+                child: Container(
+                  width: 60,
+                  child: Image.asset('assets/images/sino.png'),
+                ),
+              ),
+            ),
+          ],
+        ),
            Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 60,
-                  
-                  child:  Image.asset('assets/images/estrela.png'),
-                  
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 60,
-                  
-                  child:  Image.asset('assets/images/coracao.png'),
-                  
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 60,
-                  
-                  child:  Image.asset('assets/images/sino.png'),
-                  
-                ),
-              ),
+               GestureDetector(
+              onTap: () {
+            Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => Prateleira()),
+                );
+              },
+              child: Text('Minha'),
+            ),
+            GestureDetector(
+              onTap: () {
+            Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => Favoritos()),
+                );
+              },
+              child: Text('Produtos'),
+            ),
+            GestureDetector(
+              onTap: () {
+            Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => Alertas()),
+                );
+              },
+              child: Text('Meus'),
+             ),
             ],
            ),
 
            Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Text('Minha'),
-              Text('Produtos'),
-              Text('Meus '),
-            ],
-           ),
-           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text('Prateleira'),
-              Text('Favoritos    '),
-              Text('Alertas'),
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: GestureDetector(
+              onTap: () {
+            Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => Prateleira()),
+                );
+              },
+                
+                child: Text('Prateleira'),
+              ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 60),
+                child:GestureDetector(
+              onTap: () {
+            Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => Favoritos()),
+                );
+              },
+                child: Text('Favoritos'),
+              ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 69),
+                child: GestureDetector(
+              onTap: () {
+            Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => Prateleira()),
+                );
+              },
+                child: Text('Alertas'),
+              ),
+              ),
             ],
            ),
 
@@ -128,12 +199,82 @@ class _ContaState extends State<Conta> {
           ),
          ),
         ),
-
+        
         Container(
-          width: MediaQuery.of(context).size.width,
-         height: 350,
-         color: Color.fromARGB(31, 114, 113, 113),          
+          width: double.infinity,
+         height: 432,
+         color: Color.fromARGB(31, 114, 113, 113),
+
+         child: Padding(
+           padding: const EdgeInsets.all(16.0),
+           child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5)
+            ),
+            child: Column(
+            children: [
+              
+              ListTile(
+                leading: Icon(Icons.notifications_active, color: Colors.grey),
+                title: Text('Ativar Notificações'),
+                onTap: () {
+                 
+                },
+              ),
+              Divider(height: 1),
+              ListTile(
+                leading: Icon(Icons.shopping_cart, color: Colors.grey),
+                title: Text('Histórico de pedidos'),
+                onTap: () {
+                 
+                },
+              ),
+              Divider(height: 1),
+              ListTile(
+                leading: FaIcon(FontAwesomeIcons.ticket, color: Colors.grey),
+                title: Text('Cupons'),
+                onTap: () {
+                  
+                },
+              ),
+              Divider(height: 1),
+              ListTile(
+                leading: FaIcon(FontAwesomeIcons.fileEdit, color: Colors.grey),
+                title: Text('Dados de Cadastro'),
+                onTap: () {
+                  
+                },
+              ),
+              Divider(height: 1),
+              ListTile(
+                leading: FaIcon(FontAwesomeIcons.mapLocationDot, color: Colors.grey),
+                title: Text('Gerenciar endereços'),
+                onTap: () {
+                  
+                },
+              ),
+              Divider(height: 1),
+              ListTile(
+                leading: FaIcon(FontAwesomeIcons.asterisk, color: Colors.grey),
+                title: Text('Trocar sua senha'),
+                onTap: () {
+                  
+                },
+              ),
+              Divider(height: 1),
+              ListTile(
+                leading: FaIcon(FontAwesomeIcons.signOutAlt, color: Colors.grey),
+                title: Text('Sair'),
+                onTap: () {
+                  
+                },
+              ),
+            ],
+          ),
         ),
+           ),
+         ),        
                      Fim(),
        ]
       ),
